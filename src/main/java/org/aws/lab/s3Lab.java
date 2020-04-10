106d0d35e08e9aa2bc04f5a569e3902b9b1a5acf
Alan Tang <alan.tang@ny.frb.org>
2020-04-10 15:14:51 -0400

    get the gitIgnore right

--- src/main/java/org/aws/lab/s3Lab.java
+++ src/main/java/org/aws/lab/s3Lab.java
@@ -9,24 +9,41 @@ import com.amazonaws.services.s3.model.Bucket;
 
 public class s3Lab extends Base{
 
+    private AmazonS3 s3Client;
+
     public void run(){
-        AmazonS3 s3Client = AmazonS3ClientBuilder.standard()
+        s3Client = AmazonS3ClientBuilder.standard()
                                 .withClientConfiguration(clientConfig)
                                 .withCredentials(credProvider)
                                 .withRegion(region)
                                 .build();
 
-        // s3Client.createBucket("hr-files-z");
+        listBuckets();
+        // createBuckets();
 
+    }
+
+    public void listBuckets(){
         List<Bucket> buckets = s3Client.listBuckets();
         pr("-- list of buckets:");
         buckets.forEach( b -> pr(b.getName()) );
-        
+    }
+
+    public void uploadObjects(){
         File file = new File("c:\\temp\\employeelist.txt");
         String key = "employeeList";
         s3Client.putObject("hr-files-z", key, file);
-
         pr("file uploaded with key " + key);
+
+    }
+
+    public void createBuckets(){
+        for(int i=0; i<2; i++){
+            String bucketName = "z2note-bundles-" + i;
+            Bucket bucket = s3Client.createBucket(bucketName);
+            pr("Bucket created: " + bucket==null ? "null" : bucket.getName());
+        }       
+        
 
     }
 
